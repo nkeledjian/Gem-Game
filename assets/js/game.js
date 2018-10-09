7ac70282cadc539633610affd337d938d3f4748e
nVar <nkeledjian@gmail.com>
2018-10-08 18:18:40 -0700

    Implemented bootstrap styling and html structuring, major overhaul of javascript to jquery

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,65 +1,58 @@
 // Crystal game
-
-// Default scores at game start
-var wins = 0;
-var loses = 0;
+$(document).ready(function() {
+// Declare all variables
+  var wins = 0;
+  var losses = 0;
+  var score = 0;
+  var matchNum = 0;
+  var winsCount = $("#wins");
+  var lossesCount = $("#losses");
+  var gemVals=[0, 0, 0, 0];
+  var scoreCount=$("#score");
+  var matchNumCount=$("#matchNumCount")
 // Player's current score
-var score = [];
-
-// generate random number for player to match with
-var gameNum = Math.floor(Math.random() * (70 - 40) + 40);
-var game = gameNum;
-
-
-
-// DISPLAY ITEMS
-var disp = document.getElementById("#numbers");
-// display generated random number
-// randNum = document.innerHTML("number");
-console.log("Game Number", gameNum);
-// display number on HTML page
-// disp.innerHTML = game;
+    function refresh(){
+      winsCount.text(wins);
+      lossesCount.text(losses);
+      matchNumCount.text(matchNum);
+      scoreCount.text(score);
+    }
 
 
-// CRYSTAL LOGIC
-// add logic to randomize the score value of the crystals for every doc reload
+// Refresh Match score and all gem values 
+// Function will provide random numbers for gems ranging from 1 to 10
+  function restart() {
+    for(var g=0; g<gemVals.length, g++) {
+      gemVals[i]=Math.floor(Math.random() * 10)) + 1);
+      }
+    // random number for matchNum ranging from 20 to 30
+      score = 0;
+      matchNum = Math.floor(Math.random() * 30) + 20);
+      refresh();
+    }
 
-// Random numbers for crystals ranging from 5 to 15
-function crysNum1() {
-    return Math.floor(Math.random() * (15 - 5) + 5);
-}
-function crysNum2() {
-    return Math.floor(Math.random() * (15 - 5) + 5);
-}
-function crysNum3() {
-    return Math.floor(Math.random() * (15 - 5) + 5);
-}
-function crysNum4() {
-    return Math.floor(Math.random() * (15 - 5) + 5);
-}
+    // Assign the values from gemVals to all 4 gems
+    $(".gem").click(function(){
+      // gem is borrowing the value produced by gemVals[i] - the given i, that is
+      var gem = gemVals[this.value];
+      score = score + gem;
+        if(score == matchNum) {
+          wins++
+          restart();
+        }
+        else if(score > matchNum) {
+          losses++;
+          restart();
+        }
+      refresh();
+    })
+  restart();
+})
 
-console.log("Number for Crystal 1", crysNum1());
-console.log("Number for Crystal 2", crysNum2());
-console.log("Number for Crystal 3", crysNum3());
-console.log("Number for Crystal 4", crysNum4());
-
-/* with each click on crystal, update score++ based on random crystNum value assigned to each cyrstal */
-
-// Assigned each crystal the crysNum() function
-$("#cryst1").click(function(){
-    crysNum1()
-});
-$("#cryst2").click(function(){
-    crysNum2()
-});
-$("#cryst3").click(function(){
-    crysNum3()
-});
-$("#cryst4").click(function(){
-    crysNum4()
-});
 
 // GAME LOGIC/OUTCOMES
 /*if player total score matches the random number from the beginning of the game, display message "You win!" - add var wins++ to tally a player win. Reset doc to preserve wins score (NOT RELOAD!) */
 
 /*else if, player score exceeds random number at beginning, display message "You lose!" - add var loses++ to tally a player lose. Reset doc to preserve loses score (NOT RELOAD!) */
+
+}) // End document ready function

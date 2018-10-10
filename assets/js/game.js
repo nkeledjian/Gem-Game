2e59529f200b07cb9bdd2252329c3d49e3fe8d0c
nVar <nkeledjian@gmail.com>
2018-10-10 15:46:31 -0700

    game.js file ready - more styling to come

--- assets/js/game.js
+++ assets/js/game.js
@@ -11,12 +11,12 @@ $(document).ready(function() {
   var matchNumCount = $("#matchNumCount")
 
 // Player's stats and match number updated and displayed 
-  function refresh() {
-    winsCount.text(wins);
-    lossesCount.text(losses);
-    matchNumCount.text(matchNum);
-    scoreCount.text(score);
-  }
+    function refresh() {
+      winsCount.text(wins);
+      lossesCount.text(losses);
+      matchNumCount.text(matchNum);
+      scoreCount.text(score);
+    }
 
 // Function provides random numbers for gems ranging from 1 to 10 
     function restart() {
@@ -24,46 +24,40 @@ $(document).ready(function() {
       // $("#gem2").score = Math.floor(Math.random()* (10 - 1)) + 1;
       // $("#gem3").score = Math.floor(Math.random()* (10 - 1)) + 1;
       // $("#gem4").score = Math.floor(Math.random()* (10 - 1)) + 1;
-      // Avoided above to avoid multiple .click(function() { }) calls
+      // Avoided above convention to avoid multiple .click(function() { }) calls
 
-      // Loops through gemVals and assigns a random value to each gem
+      // Loops through gemVals indices and assigns a random value to each gem
       for (var g = 0; g < gemVals.length; g++) {
         gemVals[g] = Math.floor(Math.random()* (10-1)) + 1;
       }
-      console.log(gemVals);
       // random number for matchNum ranging from 20 to 30 for match point
       // Player's score is reset to 0
       score = 0;
       matchNum = Math.floor(Math.random() * (65 - 20)) + 20;
-      console.log("Match number", matchNum);
       // Refresh Match score and all gem values 
       refresh(); 
     }
 
     // Assign the values from gemVals to all 4 gems
     $(".gem").click(function() {
-        var gemStone = gemVals[this.value]; 
-        // if gem id == gem1
-        // assign to gemVals
-        console.log(this);
-        console.log("Gem Values", gemVals[this.value]);
+      // this points to html elements with the class "gem"
+      /* this.value allows for accessing indices of gemVals array - gemVals array is a global variable and is already populated with random numbers via function restart*/
+        var gemStone = gemVals[this.value];
         // gem values entered and accumulate the player's score
         score += gemStone;
         // If player score matches the match number, player wins!
         if(score == matchNum) {
           wins++
-          // win message
           alert("You Win! Congrats!");
           restart();
         }
         // If player surpasses match number, player losses!
         else if(score > matchNum) {
           losses++;
-          // lose message
           alert("You Lose! Better Luck Next Time!");
           restart();
         }
-        refresh();
+      refresh();
     })
-    restart();
+  restart();
 }) // End document ready function
